a39f9307c078208482ae147f99b68c64b70fbfd8
Fay Vor <32136420+phavor@users.noreply.github.com>
2018-09-27 16:02:24 +0100

    Add themes

---
+++ src/context/ThemeProvider.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Themes } from "../themes/ThemeStyles";
+
+const { Provider, Consumer } = React.createContext();
+
+class ThemeProvider extends React.Component {
+  state = {
+    ligth: Themes.ligth,
+    dark: Themes.dark
+  };
+
+  render() {
+    return <Provider value={{ ...this.state }}>{this.props.children}</Provider>;
+  }
+}
+
+export { Consumer, ThemeProvider };
---
+++ src/themes/ThemeStyles.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+export const Themes = {
+  light: {
+    background: "#eee",
+    color: "#999",
+    button: {
+      background: "orange"
+    }
+  },
+  dark: {
+    background: "#111",
+    color: "#fcfcfc",
+    button: {
+      background: "yellow"
+    }
+  }
+};
